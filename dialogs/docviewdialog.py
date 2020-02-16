cadcc52fa41182aec201f2f87045026267dca51e
Alexander Sakhruk <saharuk.alexander@gmail.com>
2020-02-16 11:27:47 +0300

    refactoring

--- dialogs/docviewdialog.py
+++ dialogs/docviewdialog.py
@@ -1,20 +1,23 @@
-from PyQt5.QtWidgets import (QWidget, QDialogButtonBox)
+from PyQt5.QtWidgets import (QWidget, QDialogButtonBox, QComboBox, QLineEdit, QSpinBox, QListView, QPlainTextEdit)
 from dialogs import DocFormDialog
 
 
 class DocViewDialog(DocFormDialog):
-	def __init__(self, parent, model, row=None):
-		super(DocViewDialog, self).__init__(parent, model, row)
+    def __init__(self, parent, model, row=None):
+        super(DocViewDialog, self).__init__(parent, model, row)
 
-		self.setWindowTitle("Просмотр документа [%s]" % super().storageUnit())
-		#for widget in self.ui.tabWidget.findChildren(QWidget):
-		#    widget.setStyleSheet("color: black")
+        self.setWindowTitle("Просмотр документа [%s]" % super().storageUnit())
+        for widget in self.ui.tabWidget.findChildren(
+            (QComboBox, QLineEdit, QSpinBox, QListView, QPlainTextEdit)):
+                if isinstance(widget, QComboBox) or isinstance(widget, QListView):
+                	widget.setDisabled(True)
+                	widget.setStyleSheet("background-color:white;color:black")
+                else:
+                    widget.setReadOnly(True)
 
-		self.ui.tab_main.setDisabled(True)
-		self.ui.tab_add.setDisabled(True)
-		self.ui.yearInsert_pushButton.hide()
-		self.ui.yearRemove_pushButton.hide()
-		self.ui.buttonBox.button(QDialogButtonBox.Save).hide()
+        self.ui.yearInsert_pushButton.hide()
+        self.ui.yearRemove_pushButton.hide()
+        self.ui.buttonBox.button(QDialogButtonBox.Save).hide()
 
 		
 
