54786f8db420b8cc012ebfc51affa73389c4c510
Alexander Sakhruk <saharuk.alexander@gmail.com>
2020-03-03 21:19:27 +0300

    custom sort for storage unit column

--- models/docsearchproxymodel.py
+++ models/docsearchproxymodel.py
@@ -1,5 +1,6 @@
 from PyQt5.Qt import Qt
 from PyQt5.QtCore import QSortFilterProxyModel
+import re
 
 
 class DocSearchProxyModel(QSortFilterProxyModel):
@@ -12,3 +13,21 @@ class DocSearchProxyModel(QSortFilterProxyModel):
                 return item.row() + 1
 
         return super().data(item, role)
+
+    def lessThan(self, left, right):
+        # custom sort for storage unit column
+        if left.column() == 12 and right.column() == 12:
+            left_vals = re.findall(r'(\d+)', left.data())
+            right_vals = re.findall(r'(\d+)', right.data())
+
+            if len(left_vals) != len(right_vals):
+                return super().lessThan(left, right)
+
+            for i in range(0, len(left_vals)):
+                if left_vals[i] != right_vals[i]:
+                    return int(left_vals[i]) > int(right_vals[i])
+
+            return True
+
+        else:
+            return super().lessThan(left, right)
